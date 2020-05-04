be1608a9c5d487a26e31b3553902807b62b7dbf8
Milad Rastin <59770869+MiladRST@users.noreply.github.com>
2020-05-04 18:46:44 +0430

    adding all files to repository

---
+++ index.js
@@ -0,0 +1,37 @@
+const addBtn = document.querySelector(".add")
+const taskField = document.querySelector(".taskInput")
+const list = document.querySelector(".list")
+
+const ToDo = () => {
+    const taskContainer = list.appendChild(document.createElement('div'))
+        const task = taskContainer.appendChild(document.createElement('p'))
+        const doneBtn = taskContainer.appendChild(document.createElement('i'))
+        const deleteBtn = taskContainer.appendChild(document.createElement('i'))
+        deleteBtn.className = 'fas fa-trash-alt'
+        doneBtn.className = 'fas fa-check'
+        taskContainer.className = 'taskContainer'
+        task.className = 'task'
+        task.innerHTML = taskField.value.charAt(0).toUpperCase() + taskField.value.slice(1)
+        taskField.value = ''
+        deleteBtn.addEventListener('click', (e) => {
+            e.target.parentElement.remove()
+        })
+        doneBtn.addEventListener('click', (e) => {
+            e.target.previousSibling.style.textDecoration = 'line-through'
+        })
+}
+
+addBtn.addEventListener("click", () => {
+    if (taskField.value.length === 0){
+        return;
+    }else{
+        ToDo()
+    }
+});
+
+taskField.addEventListener("keypress" , (event)=>{
+    if( taskField.value.length > 0 && event.keyCode === 13){
+        ToDo();
+    }
+})
+
